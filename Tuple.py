3ed4b2950151d44a0abe0189294f23550157d408
rickysiauw <37130004+rickysiauw@users.noreply.github.com>
2018-04-11 15:47:02 +0700

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,11 @@
+tupA=(1,2,3,4)
+tupB=(5,6,7,8)
+tupC=tupA+tupB
+tupD=tupC*2
+
+print "\nMenampilkan TupD : ",tupD
+print "\nCetak Element ke-3 TupD: ",tupD[3]
+print "\nCetak 3 Element Terakhir TupD: ",tupD[-3:]
+print "\nCetak Panjang tupD: ",len(tupD)
+
+
